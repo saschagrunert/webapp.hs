66879085ab9477933b27fe1a2cb620eb74150e69
Sascha Grunert <s.grunert@quapona.com>
2018-08-24 23:35:11 +0200

    Switch to reflex frp

--- default.nix
+++ default.nix
@@ -1,12 +1,11 @@
-{ mkDerivation, base, hpack, stdenv }:
-mkDerivation {
-  pname = "webapp-common";
-  version = "0.1.0";
-  src = ./.;
-  libraryHaskellDepends = [ base ];
-  libraryToolDepends = [ hpack ];
-  preConfigure = "hpack";
-  homepage = "https://github.com/saschagrunert/webapp.hs#readme";
-  description = "An approach in bringing yesod and miso together";
-  license = stdenv.lib.licenses.mit;
-}
+(import ./reflex {}).project ({ pkgs, ... }: {
+  packages = {
+    common = ./common;
+    backend = ./backend;
+    frontend = ./frontend;
+  };
+  shells = {
+    ghc = ["common" "backend" "frontend"];
+    ghcjs = ["common" "frontend"];
+  };
+})
